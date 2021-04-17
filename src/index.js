41d8a70172505f54b3c9b578ade23bfdf07dc074
devneto <miguel.neto320@gmail.com>
2021-04-17 15:39:31 -0300

    change: remove lines

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import loadable from '@loadable/component'
 
-const Login = loadable(() => import('./views/Login'))
-const SignUp = loadable(() => import('./views/SignUp'))
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
         <Route path="/" exact={true} component={App} />
-        <Route path="/login" component={Login} />
-        <Route path="/cadastro" component={SignUp} />
+        <Route path="/login" component={loadable(() => import('./views/Login'))} />
+        <Route path="/cadastro" component={loadable(() => import('./views/SignUp'))} />
       </Switch>
     </ BrowserRouter>
 
